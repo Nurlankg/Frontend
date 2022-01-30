aa59b04a3814d69aa25784403375ad47dfb5642d
Nurlankg <tch.nurlan@gmail.com>
2022-01-31 02:48:04 +0600

    ДЗ Урок 29

---
+++ JS/Urok29/script/add_to_cart.js
@@ -0,0 +1,11 @@
+let ttl = document.getElementsByClassName("ttl");
+console.log(ttl[1].innerText);
+
+let price = document.getElementsByClassName("price");
+console.log(price[0].innerText);
+
+let img1 = document.getElementsByClassName("img1");
+console.log(img1[0]);
+
+let reviews = document.getElementsByClassName("reviews");
+console.log(reviews[0].innerText);
