29b480972192ff345e82f25ee4d226a1cbc5d543
Nurlankg <tch.nurlan@gmail.com>
2022-02-01 03:05:16 +0600

    ДЗ Урок 31

---
+++ JS/Urok31/radius.js
@@ -0,0 +1,55 @@
+// let ttl = document.querySelectorAll(".body_product > .ttl");
+// console.log(ttl);
+
+// for (let text of ttl){
+//     console.log(text);
+// }
+
+// var elements = document.getElementsByClassName("ul_li");
+
+// var myFunction = function() {
+//     var attribute = this.parentNode.parentNode.querySelectorAll(".price");
+//     console.log(attribute[0].innerText);
+// };
+
+// for (var i = 0; i < elements.length; i++){
+//     elements[i].addEventListener('click', myFunction, false);
+// }
+
+// var myFunction1 = function() {
+//     var attribute = this.parentNode.parentNode.parentNode.querySelectorAll(".ttl");
+//     console.log(attribute[1].innerText);
+// };
+
+// for (var i = 0; i < elements.length; i++){
+//     elements[i].addEventListener('click', myFunction1, false);
+// }
+
+// function Radius() {
+//     let radius = prompt('Введите значение радиуса');
+//     let result = radius * 2;
+//     alert(result);
+// }
+// Radius();
+
+function stepen() {
+    let stepen = prompt('Введите число');
+    let result = stepen * stepen;
+    alert(result);
+}
+stepen();
+
+function Radius() {
+    let radius = prompt('Введите значение радиуса');
+    let result = radius * 2 * 3.14;
+    alert('Площадь круга'+ result);
+}
+Radius();
+
+function trugol() {
+    let a = prompt('введите значение длины');
+    let b = prompt('введите значение ширины');
+    let result = a * b;
+    alert('Площадь прямоугольника' + result);
+}
+trugol();
