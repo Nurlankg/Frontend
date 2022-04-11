584c56698c52ec4cf4954ff082c510e449b19fa4
Nurlan Orozmambetov <tch.nurlan@gmail.com>
2022-04-12 05:05:10 +0600

    ДЗ Урок 43

---
+++ JS/Urok43/script.js
@@ -0,0 +1,17 @@
+let click = document.getElementById("btn1");
+
+click.onclick = function (red) {
+    document.body.style.background = "red";
+}
+
+let click2 = document.getElementById("btn2");
+
+click2.onclick = function (red) {
+    document.body.style.background = "orange";
+}
+
+let click3 = document.getElementById("btn3");
+
+click3.onclick = function (red) {
+    document.body.style.background = "green";
+}
