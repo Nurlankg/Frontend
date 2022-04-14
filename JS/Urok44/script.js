285a357e72c33eb7cf5ed7db41f609b907238269
Nurlan Orozmambetov <tch.nurlan@gmail.com>
2022-04-15 01:27:41 +0600

    ДЗ Урок 44

---
+++ JS/Urok44/script.js
@@ -0,0 +1,13 @@
+function creteUser(name,birthdate,birthplace){
+    this.name = name,
+    this.birthdate = birthdate,
+    this.birthplace = birthplace;
+}
+
+let amantur = new creteUser("Amantur",1998,"Bishkek");
+console.log(amantur);
+
+let adilet = new creteUser("Adilet",1995,"Tokmok");
+console.log(adilet);
+
+
